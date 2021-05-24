36d879f0dfbba892a10b9ff97af333fea4d92184
한재원 <hanjo8813@gmail.com>
2021-05-24 14:54:36 +0900

    refactor : sklearn 버전 맞춰서 다시 pkl 생성

---
+++ SVM/refactor_model.py
@@ -0,0 +1,74 @@
+import os
+import librosa
+import numpy as np
+
+def getMFCC(i):
+  signal, sr = librosa.load(i, sr=16000)
+
+  # MFCC를 통한 특징 벡터 추출 (n_mfcc=36)
+  MFCCs = librosa.feature.mfcc(signal, sr, n_fft=400, hop_length=160, n_mfcc=36)
+  return MFCCs
+
+# 내 데이터셋의 절대경로 지정
+file_path = 'C:/Users/NB1/Desktop/PROGRAM/GitWorkSpace/CapstoneDesign_2021/Emotion-recognition/dataset/cut4/'
+file_names = os.listdir(file_path)
+
+files = []
+for file_name in file_names:
+    files.append(file_path+file_name)
+
+
+dataset=[]
+label=[] 
+
+for i in files:
+    mfcc=getMFCC(i)
+    dataset.append(mfcc.reshape(-1)) # 1차원 배열로 반환
+    label.append((os.path.basename(i)).split('_')[1]) # 파일의 이름에서 감정을 나타내는 부분을 추출해서 저장
+
+
+print('------------ 전처리 완료 ------------ ')
+
+# 라벨 인코딩
+from sklearn.preprocessing import LabelEncoder
+
+encoder = LabelEncoder()
+
+encoder.fit(label)
+y = encoder.transform(label)
+
+
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 학습 데이터와 훈련 데이터 split
+X_train, X_test, y_train, y_test = train_test_split(dataset, y, test_size = 0.2, random_state = 42, shuffle = True)
+
+# 커널 설정
+clf = SVC(C=10, kernel = 'rbf', probability=True)
+
+# MinMax Scaler를 통해 특징 벡터 전처리
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# 조정된 데이터로 SVM 학습
+clf.fit(X_train_scaled, y_train)
+
+# 스케일 조정된 테스트 세트의 정확도
+print(clf.score(X_test_scaled, y_test))
+
+#import pickle
+import joblib
+
+# 학습한 모델 파일로 저장
+joblib.dump(clf, 'model.pkl')
+
+
+# 파일로 저장된 모델 불러와서 예측
+clf_from_joblib = joblib.load('model.pkl') 
+clf_from_joblib.predict(X_test_scaled)
+

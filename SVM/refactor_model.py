d5a3d1e06127a67fe8650d93954b9c0be83c80f5
한재원 <hanjo8813@gmail.com>
2021-05-24 21:54:12 +0900

    refactor : 테스트 코드 리팩토링 + pkl 테스트 성공

--- SVM/refactor_model.py
+++ SVM/refactor_model.py
@@ -1,33 +1,27 @@
 import os
 import librosa
-import numpy as np
-
-def getMFCC(i):
-  signal, sr = librosa.load(i, sr=16000)
-
-  # MFCC를 통한 특징 벡터 추출 (n_mfcc=36)
-  MFCCs = librosa.feature.mfcc(signal, sr, n_fft=400, hop_length=160, n_mfcc=36)
-  return MFCCs
+import joblib
 
 # 내 데이터셋의 절대경로 지정
 file_path = 'C:/Users/NB1/Desktop/PROGRAM/GitWorkSpace/CapstoneDesign_2021/Emotion-recognition/dataset/cut4/'
 file_names = os.listdir(file_path)
-
 files = []
 for file_name in file_names:
     files.append(file_path+file_name)
 
+print(' -------------------------------------------- Dataset 경로 저장 완료 -------------------------------------------- ')
 
 dataset=[]
 label=[] 
 
-for i in files:
-    mfcc=getMFCC(i)
+for file in files:
+    signal, sr = librosa.load(file, sr=16000)
+    mfcc= librosa.feature.mfcc(signal, sr, n_fft=400, hop_length=160, n_mfcc=36)
     dataset.append(mfcc.reshape(-1)) # 1차원 배열로 반환
-    label.append((os.path.basename(i)).split('_')[1]) # 파일의 이름에서 감정을 나타내는 부분을 추출해서 저장
+    label.append((os.path.basename(file)).split('_')[1]) # 파일의 이름에서 감정을 나타내는 부분을 추출해서 저장
 
 
-print('------------ 전처리 완료 ------------ ')
+print(' -------------------------------------------- 전처리 완료 -------------------------------------------- ')
 
 # 라벨 인코딩
 from sklearn.preprocessing import LabelEncoder
@@ -55,20 +49,21 @@ scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
+print(' -------------------------------------------- scaler 학습 완료 -------------------------------------------- ')
+
 # 조정된 데이터로 SVM 학습
 clf.fit(X_train_scaled, y_train)
 
 # 스케일 조정된 테스트 세트의 정확도
 print(clf.score(X_test_scaled, y_test))
 
-#import pickle
-import joblib
+print(' -------------------------------------------- 모델 학습 완료 -------------------------------------------- ')
 
 # 학습한 모델 파일로 저장
-joblib.dump(clf, 'model.pkl')
+joblib.dump(clf, 'model2.pkl')
 
+print(' -------------------------------------------- pkl 저장 완료 -------------------------------------------- ')
 
 # 파일로 저장된 모델 불러와서 예측
 clf_from_joblib = joblib.load('model.pkl') 
 clf_from_joblib.predict(X_test_scaled)
-
--- SVM/test_pkl.py
+++ SVM/test_pkl.py
@@ -1,19 +1,24 @@
 import joblib
 import librosa
 
+# scaler pkl 파일 load 
+scaler_from_joblib = joblib.load('C:/Users/NB1/Desktop/PROGRAM/GitWorkSpace/CapstoneDesign_2021/Emotion-recognition/SVM/scaler.pkl')
+# clf pkl 파일 load
+clf_from_joblib = joblib.load('C:/Users/NB1/Desktop/PROGRAM/GitWorkSpace/CapstoneDesign_2021/Emotion-recognition/SVM/model.pkl')
 
 # 데이터셋 있는곳 절대경로
 file_path = 'C:/Users/NB1/Desktop/PROGRAM/GitWorkSpace/CapstoneDesign_2021/Emotion-recognition/dataset/cut4/'
 # 테스트해볼 데이터셋 파일명
-file_names = 'cuts4_fear_M_f15_1.wav'
+file_names = 'cuts2_fear_M_f5_2.wav'
 file = file_path + file_names
 
+# 음성파일 전처리 진행 
+# load -> mfcc추출 -> scale 변환
 signal, sr = librosa.load(file, sr=16000)
 mfcc = librosa.feature.mfcc(signal, sr, n_fft=400, hop_length=160, n_mfcc=36)
-mfcc = mfcc.reshape(1, -1)
+mfcc = mfcc.reshape(-1)
+mfcc = scaler_from_joblib.transform([mfcc])
 
-# pkl 파일 load
-clf_from_joblib = joblib.load('C:/Users/NB1/Desktop/PROGRAM/GitWorkSpace/CapstoneDesign_2021/Emotion-recognition/SVM/model.pkl')
+# 결과는?
 result = clf_from_joblib.predict(mfcc)
-
-print(result)
+print(result[0])
